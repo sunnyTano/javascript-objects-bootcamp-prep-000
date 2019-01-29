d22239a91978ec5b1abfcf2138521fc0f873722a
Susan Tan <susan.tan198@gmail.com>
2019-01-29 18:24:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  return delete playlist.artistName
+  return delete playlist[artistName]
 }
 
 /*

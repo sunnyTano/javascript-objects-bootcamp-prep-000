6ae45ab146e890d57f7fa2fa4edf0a5e5098b73c
Susan Tan <susan.tan198@gmail.com>
2019-01-29 18:22:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,10 @@ function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign(playlist, {[artistName]: songTitle})
 }
 
+function removeFromPlaylist(playlist, artistName){
+  return delete 
+}
+
 /*
 
 

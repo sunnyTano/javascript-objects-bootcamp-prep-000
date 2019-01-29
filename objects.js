9c0f822c269b6634a5cf3a0dad8b4e3953a80929
Susan Tan <susan.tan198@gmail.com>
2019-01-29 18:17:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,20 +8,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 /*
-describe('objects', () => {
-  const babelResult = babel.transformFileSync(
-    path.resolve(__dirname, '..', 'objects.js'), {
-      presets: ['es2015']
-    }
-  )
-  jsdom({
-    src: babelResult.code
-  })
 
-  it('defines an object called `playlist` containing at least one artist-song pair', () => {
-    expect(typeof playlist).to.equal('object')
-    expect(Object.keys(playlist).length).to.be.greaterThan(0)
-  })
 
   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
     before(() => {
